75fa690d9174d9d2d1f39215a8aa38938d673e94
FloraYin <flora8854516@gmail.com>
2023-04-01 17:55:24 +0800

    Import fonts

--- src/pages/Achievement/Achievement.tsx
+++ src/pages/Achievement/Achievement.tsx
@@ -1,10 +1,18 @@
-import Gallery from './Gallery';
+import styled from 'styled-components';
+
+// import Gallery from './Gallery';
 import Report from './Report';
+
+const Title = styled.h1`
+  font-size: 4rem;
+`;
 
 function Achievement() {
   return (
     <>
-      <h1>Achievement</h1>
+      <Title>Achievement</Title>
+      <button>Gallery</button>
+      <button>Report</button>
       {/* <Gallery /> */}
       <Report />
     </>
