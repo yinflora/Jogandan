2aae754c8ce79318d46f596722ee287ac0899dc7
FloraYin <flora8854516@gmail.com>
2023-04-01 17:03:06 +0800

    Feat: Present report according to the data

--- src/pages/Achievement/Report.tsx
+++ src/pages/Achievement/Report.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-// import { getProcessedItems } from '../../utils/firebase';
 
 const Svg = styled.svg`
   display: block;
@@ -32,6 +30,7 @@ const Rect = styled.rect`
 
 type month = string;
 type quantity = number;
+type data = number[];
 
 const MONTHS: month[] = [
   'Jan',
@@ -49,8 +48,9 @@ const MONTHS: month[] = [
 ];
 const QUANTITY: quantity[] = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
+const Items: data = [5, 23, 20, 15, 60, 78, 100, 96, 3, 34, 66, 12];
+
 export default function Report() {
-  // const [filteredItems, setFilteredItems] = useState([]);
   const X_START_AXIS: number = 50;
   const X_END_AXIS: number = 1000;
   const Y_START_AXIS: number = 50;
@@ -61,16 +61,11 @@ export default function Report() {
   const YTAG_START_AXIS: number = 550;
   const YTAG_SPACE: number = 50;
   const YTAG_X_AXIS: number = 30;
-  // const HEIGHT_PER_QTY: number = 5;
-
-  // getProcessedItems();
+  const HEIGHT_PER_QTY: number = 5;
 
   return (
     <Svg>
-      {/* <!-- X軸 --> */}
       <Line x1={X_START_AXIS} y1={Y_END_AXIS} x2={X_END_AXIS} y2={Y_END_AXIS} />
-
-      {/* <!-- X軸標籤 --> */}
       {MONTHS.map((item: month, index: number) => (
         <XTag
           key={item}
@@ -81,15 +76,12 @@ export default function Report() {
         </XTag>
       ))}
 
-      {/* <!-- Y軸 --> */}
       <Line
         x1={X_START_AXIS}
         y1={Y_START_AXIS}
         x2={X_START_AXIS}
         y2={Y_END_AXIS}
       />
-
-      {/* <!-- Y軸標籤 --> */}
       {QUANTITY.map((item: quantity, index: number) => (
         <YTag
           key={item}
@@ -100,19 +92,13 @@ export default function Report() {
         </YTag>
       ))}
 
-      {/* <!--Bar --> y從哪邊長下來 */}
-      <Rect x="100" y="525" height="25" />
-      <Rect x="175" y="200" height="300" />
-      <Rect x="250" y="150" height="350" />
-      <Rect x="325" y="450" height="50" />
-      <Rect x="400" y="375" height="125" />
-      <Rect x="475" y="245" height="255" />
-      <Rect x="550" y="275" height="225" />
-      <Rect x="625" y="135" height="365" />
-      <Rect x="700" y="135" height="365" />
-      <Rect x="775" y="135" height="365" />
-      <Rect x="850" y="135" height="365" />
-      <Rect x="925" y="135" height="365" />
+      {Items.map((item: quantity, index: number) => (
+        <Rect
+          x={XTAG_START_AXIS + index * XTAG_SPACE}
+          y={YTAG_START_AXIS - item * HEIGHT_PER_QTY}
+          height={item * HEIGHT_PER_QTY}
+        />
+      ))}
     </Svg>
   );
 }
