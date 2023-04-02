31681f77fc11071ca18e9976dd0fd36a7b547398
FloraYin <flora8854516@gmail.com>
2023-04-02 20:53:47 +0800

    Add code for drawing on canvas

--- src/utils/Hooks/useOnDraw.js
+++ src/utils/Hooks/useOnDraw.js
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 
-export function useOnDraw() {
+export function useOnDraw(onDraw) {
   const canvasRef = useRef(null);
 
   function setCanvasRef(ref) {
@@ -11,9 +11,23 @@ export function useOnDraw() {
 
   function initMouseMoveListener() {
     const mouseMoveListener = (e) => {
-      console.log({ x: e.clientX, y: e.clientY });
+      const point = computePointInCanvas(e.clientX, e.clientY);
+      const ctx = canvasRef.current.getContext('2d');
+      if (onDraw) onDraw(ctx, point);
+      console.log(point);
     };
     window.addEventListener('mousemove', mouseMoveListener);
+  }
+
+  function computePointInCanvas(clientX, clientY) {
+    if (canvasRef.current) {
+      const boundingRect = canvasRef.current.getBoundingClientRect();
+      return {
+        x: clientX - boundingRect.left,
+        y: clientY - boundingRect.top,
+      };
+    }
+    return null;
   }
 
   return setCanvasRef;
