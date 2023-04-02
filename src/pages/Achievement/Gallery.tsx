c2dedb898dfc4620d3563532dc66ab640ad35f18
FloraYin <flora8854516@gmail.com>
2023-04-02 12:42:24 +0800

    Feat: Add timeline

--- src/pages/Achievement/Gallery.tsx
+++ src/pages/Achievement/Gallery.tsx
@@ -2,18 +2,24 @@ import { useRef, useState } from 'react';
 import { flushSync } from 'react-dom';
 import styled, { css } from 'styled-components';
 
+const Container = styled.div`
+  position: relative;
+  width: 1000px;
+`;
+
 const ItemWrapper = styled.ul`
   /* max-width: 1000px; */
-  width: 1000px;
+  /* width: 1000px; */
+  width: 100%;
   display: flex;
   flex-wrap: nowrap;
   overflow-x: hidden;
-  align-items: center;
+  align-items: flex-end;
 `;
 
 const Item = styled.li`
   width: 250px;
-  /* padding: auto 10px; */
+  padding: 10px;
   flex-shrink: 0;
 
   ${({ isSelected, isFirst, isLast }) =>
@@ -31,6 +37,22 @@ const Item = styled.li`
 
 const Image = styled.img`
   width: 100%;
+`;
+
+const Time = styled.p`
+  margin: 60px auto;
+  text-align: center;
+  color: #acaea9;
+`;
+
+const Timeline = styled.div`
+  position: absolute;
+  top: 400px;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  margin-top: 30px;
+  background-color: #acaea9;
 `;
 
 export default function CatFriends() {
@@ -65,7 +87,7 @@ export default function CatFriends() {
         <button onClick={() => handlePrevious()}>Previous</button>
         <button onClick={() => handleNext()}>Next</button>
       </nav>
-      <div>
+      <Container>
         <ItemWrapper>
           {catList.map((cat, i) => (
             <Item
@@ -75,15 +97,18 @@ export default function CatFriends() {
               isFirst={index === 0}
               isLast={index === catList.length - 1}
             >
+              {/* <p>{cat.id}</p> */}
               <Image
                 // className={index === i ? 'active' : ''}
                 src={cat.imageUrl}
                 alt={'Cat #' + cat.id}
               />
+              <Time>2023/04/01</Time>
             </Item>
           ))}
         </ItemWrapper>
-      </div>
+        <Timeline />
+      </Container>
     </>
   );
 }
@@ -92,6 +117,7 @@ const catList = [];
 for (let i = 0; i < 10; i++) {
   catList.push({
     id: i,
+    // time: new Date(),
     imageUrl: 'https://placekitten.com/250/200?image=' + i,
   });
 }
