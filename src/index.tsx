91cfdce26e84d59387aafa6e43653a4b66f7b244
FloraYin <flora8854516@gmail.com>
2023-04-03 14:24:06 +0800

    Fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Achievement from './pages/Achievement/Achievement';
 import Compose from './pages/Compose/Compose';
+import Profile from './pages/Profile/Profile';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,6 +15,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />}>
         {/* <Route index element={<Home />} /> */}
+        <Route path="profile" element={<Profile />} />
         <Route path="achievement" element={<Achievement />} />
         <Route path="compose" element={<Compose />} />
         <Route path="*" element={<Navigate to="/" replace />} />
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,3 @@
+export default function Profile() {
+  return <>Hi我是登入</>;
+}
