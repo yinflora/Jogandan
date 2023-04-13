560c813e4a5efa069e875ea0a72d0ddc06f8e705
FloraYin <flora8854516@gmail.com>
2023-04-13 18:18:50 +0800

    Refactor Typescript

--- src/pages/Inventory/Popout.tsx
+++ src/pages/Inventory/Popout.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Timestamp } from 'firebase/firestore';
 
 import EditItem from '../Upload/Upload';
 import { useState } from 'react';
@@ -21,18 +22,11 @@ const Overlay = styled.div`
 `;
 
 const Cancel = styled(Link)`
-  /* width: 20px;
-  height: 10px; */
-  /* transform: scale(2, 0.5); */
   margin-left: auto;
-  /* width: 100%; */
-  /* text-align: end; */
   color: #f1f2ed;
 `;
 
 const Container = styled.div`
-  /* width: 70%;
-  height: 80%; */
   display: flex;
   width: 100%;
   height: 100%;
@@ -43,10 +37,6 @@ const Container = styled.div`
 
 const ImageWrapper = styled.div`
   width: 40%;
-  /* display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 80% 20%; */
-  /* background-color: gray; */
 `;
 
 const MainImage = styled.img`
@@ -59,15 +49,12 @@ const MainImage = styled.img`
 
 const SubImageWrapper = styled.div`
   display: flex;
-  /* justify-content: space-between; */
   overflow-x: scroll;
   flex-wrap: nowrap;
-  /* gap: 10px; */
 `;
 
 const SubImage = styled.img`
   width: calc(100% / 3);
-  /* width: 25%; */
   padding: 5px;
   object-fit: cover;
   object-position: center;
@@ -80,14 +67,10 @@ const InfoWrapper = styled.div`
   width: 60%;
   flex-direction: column;
   justify-content: space-between;
-  /* background-color: gray; */
   color: #acaea9;
 `;
 
-const Category = styled.span`
-  /* color: #acaea9; */
-  /* margin-right: auto; */
-`;
+const Category = styled.span``;
 
 const Name = styled.p`
   font-size: 2rem;
@@ -102,61 +85,54 @@ const Row = styled.div`
 const Title = styled.span`
   display: inline-block;
   width: 100px;
-  /* color: #acaea9; */
 `;
 
-const Content = styled.span`
-  /* color: #acaea9; */
-`;
+const Content = styled.span``;
 
 const Description = styled.div`
   padding: 20px 0;
   overflow-y: scroll;
 `;
 
-const Edit = styled.button`
-  /* margin-left: auto; */
-  /* text-align: end; */
-`;
+const Edit = styled.button``;
 
 const FirstRow = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
+type Item = {
+  id: string;
+  name: string;
+  status: string;
+  category: string;
+  created: Timestamp;
+  processedDate: string;
+  description: '';
+  images: string[];
+};
+
 type PopoutProp = {
-  // setIsPopout: React.Dispatch<React.SetStateAction<boolean>>;
-  selectedItem: [] | null;
+  selectedItem: Item | null;
 };
 
 export default function Popout({ selectedItem }: PopoutProp) {
   const [isEdit, setIsEdit] = useState<boolean>(false);
 
-  function formatTime(time) {
-    const date = new Date(time * 1000); // 轉換成毫秒
-    const options = {
-      // year: 'numeric',
-      // month: 'long',
-      // day: 'numeric',
-      // hour: 'numeric',
-      // minute: 'numeric',
-      // second: 'numeric',
-      // hour12: false, // 24小時制
-      // timeZoneName: 'short',
+  function formatTime(time: number) {
+    const date = new Date(time * 1000);
+    const options: object = {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
     };
-    return date.toLocaleDateString('zh-TW', options); // 繁體中文語系
+    return date.toLocaleDateString('zh-TW', options);
   }
 
-  if (selectedItem) {
-    // if (isEdit) {
-    //   return <EditItem />;
-    // }
+  if (Array.isArray(selectedItem) && selectedItem.length > 0) {
+    const firstItem = selectedItem[0];
     return (
       <Overlay>
-        {/* <Cancel onClick={() => setIsPopout(false)}>X</Cancel> */}
         <Cancel to="/inventory">X</Cancel>
 
         {isEdit ? (
@@ -164,39 +140,36 @@ export default function Popout({ selectedItem }: PopoutProp) {
         ) : (
           <Container>
             <ImageWrapper>
-              <MainImage src={selectedItem[0].images[0]} />
+              <MainImage src={firstItem.images[0]} />
               <SubImageWrapper>
-                {selectedItem[0].images.map(
-                  (image) =>
+                {firstItem.images.map(
+                  (image: string) =>
                     image !== '' && <SubImage key={image} src={image} />
                 )}
               </SubImageWrapper>
             </ImageWrapper>
             <InfoWrapper>
               <FirstRow>
-                <Category>{selectedItem[0].category}</Category>
-                {selectedItem[0].status !== '已處理' && (
+                <Category>{firstItem.category}</Category>
+                {firstItem.status !== '已處理' && (
                   <Edit onClick={() => setIsEdit(true)}>Edit</Edit>
                 )}
               </FirstRow>
-              <Name>{selectedItem[0].name}</Name>
+              <Name>{firstItem.name}</Name>
               <Row>
                 <Title>購買日期</Title>
-                <Content>{formatTime(selectedItem[0].created.seconds)}</Content>
+                <Content>{formatTime(firstItem.created.seconds)}</Content>
               </Row>
               <Row>
                 <Title>目前狀態</Title>
-                <Content>{selectedItem[0].status}</Content>
+                <Content>{firstItem.status}</Content>
               </Row>
-              {/* <Row>
-              <Title>數量</Title>
-              <Content>1</Content>
-            </Row> */}
-              <Description>{selectedItem[0].description}</Description>
+              <Description>{firstItem.description}</Description>
             </InfoWrapper>
           </Container>
         )}
       </Overlay>
     );
   }
+  return null;
 }
