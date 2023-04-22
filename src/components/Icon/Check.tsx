fadcef8c83ddbbfea676556657213e0c4ec39a38
FloraYin <flora8854516@gmail.com>
2023-04-23 00:31:05 +0800

    Modify Spark joy

---
+++ src/components/Icon/Check.tsx
@@ -0,0 +1,19 @@
+export default function Check() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="90"
+      height="90"
+      viewBox="0 0 24 24"
+      stroke-width="1"
+      stroke="#ffffff"
+      fill="rgba(255, 255, 255, 0.1)"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <circle cx="12" cy="12" r="9" />
+      <path d="M9 12l2 2l4 -4" />
+    </svg>
+  );
+}
