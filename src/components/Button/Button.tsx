8405c2650ef74b0401dfd3536cd8322cff3525bc
FloraYin <flora8854516@gmail.com>
2023-04-22 16:47:39 +0800

    Add styles for upload

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,61 @@
-// import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components';
 
-// const StyledButton = styled.button`
-//   min-width: 200px;
-//   min-height: 50px;
-// `;
+const StyledButton = styled.button<ButtonProps>`
+  min-width: 200px;
+  min-height: 50px;
+  width: ${({ width }) => width};
 
-// export default function Button({ styles, text }) {
-//   return <StyledButton {...styles}>{text}</StyledButton>;
-// }
+  ${({ buttonType }) => {
+    if (buttonType === 'dark') {
+      return css`
+        background-color: rgb(0, 0, 0, 0.6);
+        border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+        color: #fff;
+      `;
+    } else if (buttonType === 'normal') {
+      return css`
+        background-color: #8d9ca4;
+        border-bottom: 1px solid #fff;
+        color: #fff;
+      `;
+    } else if (buttonType === 'light') {
+      return css`
+        background-color: rgba(255, 255, 255, 0.5);
+        border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+        color: #000;
+      `;
+    }
+    return null;
+  }}
 
-import React from 'react';
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
-export default function Button() {
-  return <div>Button</div>;
+type ButtonProps = {
+  width?: string;
+  buttonType: 'dark' | 'normal' | 'light';
+  children: string;
+  disabled?: boolean;
+  onClick?: () => void;
+};
+
+export default function Button({
+  width,
+  buttonType,
+  onClick,
+  children,
+  disabled,
+}: ButtonProps) {
+  return (
+    <StyledButton
+      width={width}
+      buttonType={buttonType}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </StyledButton>
+  );
 }
