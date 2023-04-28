6cfa2e40306356d6876ad21b1d13cd4bc94cf73e
FloraYin <flora8854516@gmail.com>
2023-04-28 20:46:25 +0800

    style: refactor button styles in StyledButton component
    - Add a `position` and a `z-index` property to the `StyledButton`
    - Add a `cursor` property to the `&amp;:hover` selector in `StyledButton`
    - Modify the `background-color`, `border-bottom`, and `color` properties in the `dark` buttonType of `StyledButton`
    - Add a `cursor`, `background-color`, `border-bottom`, and `color` properties to the `&amp;:disabled` selector in the `dark` buttonType of `StyledButton`
    - Add a `&amp;::before` selector to the `dark` buttonType of `StyledButton`
    - Modify the `background-color`, `border-bottom`, and `color` properties in the `normal` buttonType of `StyledButton`
    - Add a `&amp;::before` selector to the `normal` buttonType of `StyledButton`
    - Remove the `background-color` property in the `light` buttonType of `StyledButton`
    - Add a `
    
    Signed-off-by: FloraYin <flora8854516@gmail.com>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import styled, { css } from 'styled-components';
 
 const StyledButton = styled.button<ButtonProps>`
+  position: relative;
+  z-index: 0;
   min-width: 200px;
   min-height: 50px;
   width: ${({ width }) => width};
   font-size: 1rem;
   letter-spacing: 0.1rem;
+
+  &:hover {
+    cursor: pointer;
+  }
 
   ${({ buttonType }) => {
     if (buttonType === 'dark') {
@@ -13,28 +19,102 @@ const StyledButton = styled.button<ButtonProps>`
         background-color: rgb(0, 0, 0, 0.6);
         border-bottom: 1px solid rgba(0, 0, 0, 0.5);
         color: #fff;
+
+        &:disabled {
+          cursor: not-allowed;
+          background-color: rgb(0, 0, 0, 0.3);
+          border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+          color: #8d9ca4;
+        }
+
+        &::before {
+          content: '';
+          position: absolute;
+          z-index: -1;
+          top: 0;
+          bottom: 0;
+          right: 100%;
+          left: 0;
+          /* border-bottom: 2px solid #fff; */
+          background-color: #000;
+          opacity: 0;
+          transition: all 0.5s;
+        }
+
+        /* &:hover::before {
+          left: 0;
+          right: 0;
+          opacity: 1;
+        } */
+
+        &:hover:not(:disabled)::before {
+          left: 0;
+          right: 0;
+          opacity: 1;
+        }
       `;
     } else if (buttonType === 'normal') {
       return css`
         background-color: #8d9ca4;
         border-bottom: 1px solid #fff;
         color: #fff;
+
+        &::before {
+          content: '';
+          position: absolute;
+          z-index: -1;
+          top: 0;
+          bottom: 0;
+          right: 100%;
+          left: 0;
+          border-bottom: 2px solid #fff;
+          background-color: #646e74;
+          opacity: 0;
+          transition: all 0.5s;
+        }
+
+        &:hover::before {
+          left: 0;
+          right: 0;
+          opacity: 1;
+        }
       `;
     } else if (buttonType === 'light') {
       return css`
         background-color: rgba(255, 255, 255, 0.5);
         border-bottom: 1px solid rgba(0, 0, 0, 0.5);
         color: #000;
+
+        &:disabled {
+          cursor: not-allowed;
+          background-color: rgb(0, 0, 0, 0.3);
+          border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+          color: #8d9ca4;
+        }
+
+        &::before {
+          content: '';
+          position: absolute;
+          z-index: -1;
+          top: 0;
+          bottom: 0;
+          right: 100%;
+          left: 0;
+          border-bottom: 2px solid #000;
+          background-color: #fff;
+          opacity: 0;
+          transition: all 0.5s;
+        }
+
+        &:hover:not(:disabled)::before {
+          left: 0;
+          right: 0;
+          opacity: 1;
+        }
       `;
     }
     return null;
-  }}
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  ${({ disabled }) =>
+  }}/* ${({ disabled }) =>
     disabled &&
     css`
       &:hover {
@@ -43,7 +123,7 @@ const StyledButton = styled.button<ButtonProps>`
         border-bottom: 1px solid rgba(0, 0, 0, 0.5);
         color: #cdcdcd;
       }
-    `}
+    `} */
 `;
 
 type ButtonProps = {
